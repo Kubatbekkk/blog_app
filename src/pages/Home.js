3c8039dfb1f06df71a25735b82695862c3dd3e99
Kubatbekkk <kubineccastro@gmail.com>
2022-03-05 20:42:16 +0600

    added pagination with react-paginate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
 import { auth, db } from "../firebase-config";
+import ReactPaginate from "react-paginate";
 
 function Home({ isAuth }) {
   const [postLists, setPostList] = useState([]);
   const postsCollectionRef = collection(db, "posts");
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const postsPerPage = 3;
+  const pagesVisited = pageNumber * postsPerPage;
 
   useEffect(() => {
     const getPosts = async () => {
@@ -20,35 +25,54 @@ function Home({ isAuth }) {
     const postDoc = doc(db, "posts", id);
     await deleteDoc(postDoc);
   };
-  return (
-    <div className="homePage">
-      {postLists.map((post) => {
-        return (
-          <div className="post" key={post.id}>
-            <div className="postHeader">
-              <div className="title">
-                <h1> {post.title}</h1>
-              </div>
-              <div className="deletePost">
-                {isAuth && post.author.id === auth.currentUser.uid && (
-                  <button
-                    onClick={() => {
-                      deletePost(post.id);
-                    }}
-                  >
-                    {" "}
-                    x
-                  </button>
-                )}
-              </div>
-            </div>
-            <div className="postTextContainer"> {post.postText} </div>
-            <h3>@{post.author.name}</h3>
+
+  const displayPosts = postLists.slice(pagesVisited, pagesVisited + postsPerPage).map(post => {
+    return (
+      <div className="post" key={post.id}>
+        <div className="postHeader">
+          <div className="title">
+            <h1> {post.title}</h1>
           </div>
-        );
-      })}
+          <div className="deletePost">
+            {isAuth && post.author.id === auth.currentUser.uid && (
+              <button
+                onClick={() => {
+                  deletePost(post.id);
+                }}
+              >
+                {" "}
+                x
+              </button>
+            )}
+          </div>
+        </div>
+        <div className="postTextContainer"> {post.postText} </div>
+        <h3>@{post.author.name}</h3>
+      </div>
+    );
+  })
+  const pageCount = Math.ceil(postLists.length / postsPerPage);
+  const changePage = ({ selected }) => {
+    setPageNumber(selected)
+  }
+
+  return (
+    <div className="homePage"> 
+      {displayPosts}
+      <ReactPaginate 
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={"paginationBttns"}
+        previousLinkClassName={"previousBttn"}
+        nextLinkClassName={"nextBttn"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+      />
+
     </div>
   );
-}
+};
 
 export default Home;
