4b96b8875622e62ec246aeb48d94cd6662527e17
Kubatbekkk <kubineccastro@gmail.com>
2022-03-06 22:14:46 +0600

    added clap button with lyket component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import { Provider } from '@lyket/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-    <App />,
+  <Provider apiKey="pt_3d23065a0bfe9a54fa2e27433da76e">
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
 import { auth, db } from "../firebase-config";
 import ReactPaginate from "react-paginate";
+import { ClapButton } from "@lyket/react";
 
 function Home({ isAuth }) {
   const [postLists, setPostList] = useState([]);
@@ -20,13 +21,20 @@ function Home({ isAuth }) {
     getPosts();
   }, [postsCollectionRef]);
 
+  const [value, setValue] = useState('');
+
+  const filteredPosts = postLists.filter(post => {
+    return post.title.toLowerCase().includes(value.toLowerCase())
+  })
+
   // const deletePost = async 
   async function deletePost(id) {
     const postDoc = doc(db, "posts", id);
     await deleteDoc(postDoc);
   };
 
-  const displayPosts = postLists.slice(pagesVisited, pagesVisited + postsPerPage).map(post => {
+  const displayPosts = filteredPosts.slice(pagesVisited, pagesVisited + postsPerPage)
+    .map(post => {
     return (
       <div className="post" key={post.id}>
         <div className="postHeader">
@@ -47,7 +55,7 @@ function Home({ isAuth }) {
           </div>
         </div>
         <div className="postTextContainer"> {post.postText} </div>
-        <h3>@{post.author.name}</h3>
+        <div className="postAuthor"><h6>@{post.author.name}</h6><ClapButton id={post.id}/></div>
       </div>
     );
   })
@@ -57,6 +65,17 @@ function Home({ isAuth }) {
   }
 
   return (
+    <>
+    <div className="form">
+      <form className="search__form">
+        <input 
+          type="text"
+          placeholder="Search..."
+          className="search__input"
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </form>
+    </div>
     <div className="homePage"> 
       {displayPosts}
       <ReactPaginate 
@@ -70,8 +89,8 @@ function Home({ isAuth }) {
         disabledClassName={"paginationDisabled"}
         activeClassName={"paginationActive"}
       />
-
     </div>
+    </>
   );
 };
 
